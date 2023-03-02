8002c70aac35de99fa83d17282026ed93e670b5f
James <james@Zipcoders-MacBook-Pro-101.local>
2023-03-02 07:38:40 -0500

    James do the changos

--- src/main/java/com/zipcodewilmington/danny_do_better_exercises/StringUtilities.java
+++ src/main/java/com/zipcodewilmington/danny_do_better_exercises/StringUtilities.java
@@ -1,5 +1,9 @@
 package com.zipcodewilmington.danny_do_better_exercises;
 
+import org.w3c.dom.ls.LSOutput;
+
+import java.nio.file.LinkPermission;
+
 /**
  * Created by dan on 6/14/17.
  */
@@ -8,81 +12,72 @@ public class StringUtilities {
      * @return `Hello World` as a string
      */
     public static String getHelloWorld() {
-        return null;
+        return "Hello World";
     }
-
-    /**
-     * @param firstSegment a string to be added to
-     * @param secondSegment a string to add
-     * @return the concatenation of two strings, `firstSegment`, and `secondSegment`
-     */
     public static String concatenation(String firstSegment, String secondSegment){
-        return null;
+        return firstSegment + secondSegment;
     }
-
     /**
      * @param firstSegment a string to be added to
      * @param secondSegment a string to add
      * @return the concatenation of an integer, `firstSegment`, and a String, `secondSegment`
      */
     public static String concatenation(int firstSegment, String secondSegment){
-        return null;
+        return firstSegment + secondSegment;
     }
-
     /**
      * @param input a string to be manipulated
      * @return the first 3 characters of `input`
      */
     public static String getPrefix(String input){
-        return null;
+        return input.substring(0,3);
     }
-
     /**
      * @param input a string to be manipulated
      * @return the last 3 characters of `input`
      */
     public static String getSuffix(String input){
-        return null;
+        return input.substring(2,5);
     }
-
-    /**
-     * @param inputValue the value to be compared
-     * @param comparableValue the value to be compared against
-     * @return the equivalence of two strings, `inputValue` and `comparableValue`
-     */
-    public static Boolean compareTwoStrings(String inputValue, String comparableValue){
-        return null;
+    public static boolean compareTwoStrings(String inputValue, String comparableValue){
+        return inputValue.equals(comparableValue);
     }
-
-    /**
-     * @param inputValue the value input from user
-     * @return the middle character of `inputValue`
-     */
     public static Character getMiddleCharacter(String inputValue){
-        return null;
+        String str = inputValue;
+        int length = str.length();
+        if (length % 2 == 0) {
+            char ch1 = str.charAt((length/2-1));
+            return ch1;
+        } else {
+            char ch = str.charAt(length/2);
+            return ch;
+        }
     }
-
     /**
      * @param spaceDelimitedString a string, representative of a sentence, containing spaces
      * @return the first sequence of characters
      */
     public static String getFirstWord(String spaceDelimitedString){
-        return null;
+        int i = spaceDelimitedString.indexOf(" ");
+        String firstWord = spaceDelimitedString.substring(0, i);
+        return firstWord;
+        /*String arr[] = spaceDelimitedString.split(" ", 2);
+        String firstWord = arr[0];
+        return firstWord; */
     }
-
-    /**
-     * @param spaceDelimitedString a string delimited by spaces
-     * @return the second word of a string delimited by spaces.
-     */
     public static String getSecondWord(String spaceDelimitedString){
-        return null;
+        return spaceDelimitedString.split(" ") [1];
+       /* String arr[] = spaceDelimitedString.split(" ", 2);
+        String SecondWord = arr[1];
+        return SecondWord;*/
     }
-
     /**
      * @param stringToReverse
      * @return an identical string with characters in reverse order.
      */
     public static String reverse(String stringToReverse){
-        return null;
+        StringBuilder backwards = new StringBuilder(stringToReverse);
+        backwards.reverse();
+        return backwards.toString();
     }
 }
